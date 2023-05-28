4346c52d4eddad38cc859b77bf3494886080419f
Flavio Cardoso <flavioc41@gmail.com>
2023-05-28 01:22:27 -0300

    teste no botão adicionar

--- src/components/shared/AddCount/index.cy.ts
+++ src/components/shared/AddCount/index.cy.ts
@@ -1,12 +1,32 @@
 import Index from './index.vue'
 import { createPinia, setActivePinia } from 'pinia';
+import { useCount } from '@/stores/count';
 
 describe('<Index />', () => {
+  let useStore: any;
   beforeEach(() => {
     setActivePinia(createPinia())
+    useStore = useCount();
+    cy.mount(Index)
   })
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
-    cy.mount(Index)
+    cy.get('button.success').should('have.text', 'Acrescente')
+  })
+  it('clique botão', () => {
+    cy.get('button.success')
+    .click()
+    .then(() => {
+        expect(1).to.equal(useStore.count)
+    })
+    .click()
+    .then(() => {
+      expect(2).to.equal(useStore.count)
+    })
+    .click()
+    .then(() => {
+      expect(3).to.equal(useStore.count)
+    })
+
   })
 })
