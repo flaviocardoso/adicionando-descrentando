86fb12c3914a49cb8d56d8c20eee9c47f5df4d62
Flavio Cardoso <flavioc41@gmail.com>
2023-05-30 22:42:34 -0300

    construindo o twonumbers

--- src/components/shared/TwoNumbers/index.vue
+++ src/components/shared/TwoNumbers/index.vue
@@ -1,31 +1,96 @@
 <template>
-  <Card>
-    <template #title>
-      <TextCenter>TWO NUMBERS</TextCenter>
-    </template>
-    <template #content>
-      <Block> - FIRST - </Block>
-      <Block> - OPERATION - </Block>
-      <Block> - SECOND - </Block>
-      <Block> - EQUAL - </Block>
-    </template>
-    <template #footer>
-      <Block>RESULT</Block>
-    </template>
-  </Card>
+  <div class="TwoNumbers">
+    <Card>
+      <template #title>
+        <TextCenter>TWO NUMBERS</TextCenter>
+      </template>
+      <template #content>
+        <Block>
+          <input class="numero" type="number" ref="pNumero" placeholder="Primeiro número"/>
+        </Block>
+        <Block>
+          <select class="operador" ref="op">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
+        </Block>
+        <Block>
+          <input  class="numero" type="number" ref="sNumero" placeholder="Segundo número"/>
+        </Block>
+        <Block>
+          <Button type="success" @click="acaoIgual">=</Button>
+        </Block>
+      </template>
+      <template #footer>
+        <Block>
+          <span class="resultado">{{ resultado }}</span>
+        </Block>
+      </template>
+    </Card>
+  </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, ref } from 'vue';
   import Block from '../Block';
+  import Button from '../Button';
   import Card from '../Card';
   import TextCenter from '../TextCenter';
 
+  const opMathFunc = {
+    "+" : (a: any, b: any) => a + b,
+    "-" : (a: any, b: any) => a - b,
+    "*" : (a: any, b: any) => a * b,
+    "/" : (a: any, b: any) => (a / b).toFixed(2)
+  }
+
+  type OPMATH = "+" | "-" | "*" | "/";
+
+  const opMathValues = {
+    "+" : "Adicionar",
+    "-" : "Diminuir",
+    "*" : "Multiplicar",
+    "/" : "Dividir"
+  }
+
+  const opMathSign = ["+", "-", "*", "/"];
+
   export default defineComponent({
     name: "TwoNumbers",
+    setup() {
+      const pNumero = ref<HTMLInputElement>();
+      const sNumero = ref<HTMLInputElement>();
+      const op = ref<HTMLSelectElement>();
+      const resultado = ref(0);
+
+      function acaoIgual() {
+        if (op.value && opMathSign.includes(op.value?.value)) {
+          let valorOp: OPMATH = op.value.value as OPMATH;
+          resultado.value = opMathFunc[valorOp](Number(pNumero.value?.value), Number(sNumero.value?.value))
+        } else {
+          resultado.value = opMathFunc['+'](Number(pNumero.value?.value), Number(sNumero.value?.value))
+        }
+      }
+
+      return {
+        pNumero,
+        sNumero,
+        op,
+        resultado,
+        acaoIgual
+      }
+    },
+    mounted() {
+    },
     components: {
-      TextCenter,
-      Block,
-      Card
+        TextCenter,
+        Block,
+        Card,
+        Button
     }
   });
 </script>
+<style scoped lang="scss">
+  @import './_.scss';
+</style>
