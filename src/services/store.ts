7fdc0edc99167fbe82ea0ef47fcdff71fdff471f
Flavio Cardoso <flavioc41@gmail.com>
2023-06-20 22:36:30 -0300

    indentação do store e simplificando class store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,21 +1,17 @@
 
-class Store<T> {
+class Store {
   constructor(private nome: string) {}
 
-  set salvar(dados: T) {
+  set value(dados: any) {
     const dadosString = JSON.stringify(dados);
     localStorage.setItem(this.nome, dadosString);
   }
 
-  get pegar(): any {
+  get value(): any {
     const dadosString = localStorage.getItem(this.nome);
     if (dadosString) {
       return JSON.parse(dadosString);
     }
-  }
-
-  apagar(): void {
-    localStorage.removeItem(this.nome)
   }
 }
 
--- src/stores/todo.ts
+++ src/stores/todo.ts
@@ -2,8 +2,8 @@ import { acceptHMRUpdate, defineStore } from "pinia";
 import ILista from '@/interfaces/ILista';
 import Store from '@/services/store';
 
-const NOME_STORE_TODO = 'TESTE_API_LOCAL_TODO'
-const dados = new Store<ILista[]>(NOME_STORE_TODO);
+const STORE_TODO = 'APITODO'
+const todostore = new Store(STORE_TODO);
 
 const pegarUltimoItemId = (lista: ILista[]): number => {
   const lastItem = [...lista].shift();
@@ -13,55 +13,53 @@ const pegarUltimoItemId = (lista: ILista[]): number => {
 }
 
 export const useTODO = defineStore({
-    id: 'todo',
-    state: () => ({
-        lista: dados.pegar as ILista[] || [] as ILista[]
-    }),
-    actions: {
-        adicionar (item: { conteudo: string, prioridade: number }) {
-          const id = pegarUltimoItemId(this.lista);
-          const itemTODO = {
-            id: id,
-            conteudo: item.conteudo,
-            prioridade: item.prioridade,
-            feito: false
-          };
-          this.lista = [itemTODO, ...this.lista];
-          dados.salvar = this.lista
-        },
-        remover (id: number) {
-          this.lista = this.lista.filter(item => item.id !== id)
-          dados.salvar = this.lista
-        },
-        editar (id: number, conteudo: string) {
-          this.lista.map(item => {
-            if (item.id === id) {
-              item.conteudo = conteudo;
-            }
-          });
-          dados.salvar = this.lista
-        },
-        buscarConteudo (id: number): string {
-         let item = this.lista.filter(item => item.id === id);
-         return item[0].conteudo;
-        },
-        feito(id: number) {
-          this.lista.map(item => {
-            if (item.id === id) {
-              item.feito = !item.feito;
-            }
-          })
-          dados.salvar = this.lista
-        },
-        clear() {
-          this.lista = []
-          dados.apagar()
-        }
+  id: 'todo',
+  state: () => ({
+    lista: todostore.value as ILista[] || [] as ILista[]
+  }),
+  actions: {
+    adicionar (item: { conteudo: string, prioridade: number }) {
+      const id = pegarUltimoItemId(this.lista);
+      const itemTODO = {
+        id: id,
+        conteudo: item.conteudo,
+        prioridade: item.prioridade,
+        feito: false
+      };
+      todostore.value = this.lista = [itemTODO, ...this.lista];
     },
-    getters: {
-      getLista: (state) => state.lista,
-      empty: (state) => state.lista.length == 0
+    remover (id: number) {
+      todostore.value = this.lista = this.lista.filter(item => item.id !== id)
+
+    },
+    editar (id: number, conteudo: string) {
+      this.lista.map(item => {
+        if (item.id === id) {
+          item.conteudo = conteudo;
+        }
+      });
+      todostore.value = this.lista
+    },
+    buscarConteudo (id: number): string {
+      let item = this.lista.filter(item => item.id === id);
+      return item[0].conteudo;
+    },
+    feito(id: number) {
+      this.lista.map(item => {
+        if (item.id === id) {
+          item.feito = !item.feito;
+        }
+      })
+      todostore.value = this.lista
+    },
+    clear() {
+      todostore.value = this.lista = []
     }
+  },
+  getters: {
+    getLista: (state) => state.lista,
+    empty: (state) => state.lista.length == 0
+  }
 });
 
 if (import.meta.hot) {
