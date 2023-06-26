a975494b5032ea8fe764c3deb5325517ddbc9b37
Flavio Cardoso <flavioc41@gmail.com>
2023-06-25 23:22:02 -0300

    estrutura calculadora

--- src/components/shared/Calculadora/index.vue
+++ src/components/shared/Calculadora/index.vue
@@ -1,9 +1,84 @@
 <template>
   <div class="CalculadoraStyle">
-    Calculadora
+    <div class="dentro">
+      <span class="screen">
+        <span class="resultado">Resultado</span>
+        <span class="conta">Conta</span>
+      </span>
+      <span class="linha">
+        <span class="normal">
+          <span class="tecla">%</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">C</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">B</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">÷</span>
+        </span>
+      </span>
+      <span class="linha">
+        <span class="normal">
+          <span class="tecla">7</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">8</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">9</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">x</span>
+        </span>
+      </span>
+      <span class="linha">
+        <span class="normal">
+          <span class="tecla">4</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">5</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">6</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">-</span>
+        </span>
+      </span>
+      <span class="linha">
+        <span class="normal">
+          <span class="tecla">1</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">2</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">3</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">+</span>
+        </span>
+      </span>
+      <span class="linha">
+        <span class="normal">
+          <span class="tecla">+/-</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">0</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">,</span>
+        </span>
+        <span class="normal">
+          <span class="tecla">=</span>
+        </span>
+      </span>
+    </div>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 </script>
 <style scoped>
 @import './_.scss';
